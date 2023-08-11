2eff815077df2779fc9d757077358670feb141f1
Bruno Silva <silva.dev@hotmail.com>
2023-08-11 17:25:45 -0300

    Atualização js

---
+++ js/formulario.js
@@ -0,0 +1,27 @@
+const formulario = document.querySelector("form");
+
+function formularioEnviado(resposta) {
+  if (resposta.ok) {
+    formulario.innerHTML =
+      "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;>Mensagem enviada, em breve entraremos em contato!";
+  } else {
+    formulario.innerHTML =
+      "<p>Erro no envio, você pode enviar diretamente para o nosso email em: teste@teste.com</p>";
+  }
+}
+
+function enviarFormulario(event) {
+  event.preventDefault();
+  const botao = document.querySelector("form button");
+  botao.disabled = true;
+  botao.innerText = "Enviando...";
+
+  const data = new FormData(formulario);
+
+  fetch("./enviar.php", {
+    method: "POST",
+    body: data,
+  });
+}
+
+formulario.addEventListener("submit", enviarFormulario);
